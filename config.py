54d5acd2bc7469e99de4f5b032e4ee3382666c7c
Vansh kumar <vanshkumar6522@gmail.com>
2025-04-22 00:15:10 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'your_mysql_password',  # Replace with your password
+    'database': 'charity_db'
+}
+SECRET_KEY = 'any_random_string'
